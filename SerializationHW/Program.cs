ea1f9e7ade16877be2eedf2b89ce87d187235301
Nurkaiyr <yedige21@gmail.com>
2017-12-28 19:09:58 +0600

    Homework

---
+++ SerializationHW/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SerializationHW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Book> listBooks = new List<Book>()
+            {
+                new Book
+                {
+                    Name= "Джейн Эйр",
+                    Price = 1500,
+                    Author = "Шарлотта Бронте",
+                    Year = 2011,
+                }
+            };
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = new FileStream(@"C\:books.txt", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(stream, listBooks);
+            }
+
+            using (FileStream stream = new FileStream(@"C:\books.txt", FileMode.OpenOrCreate))
+            {
+                var bookList = formatter.Deserialize(stream) as List<Book>;
+            }
+
+        }
+    }
+}
